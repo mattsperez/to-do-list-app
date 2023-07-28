1c49a72ec3e53f9f8a389099ce96187e56243f94
mattsperez <131983739+mattsperez@users.noreply.github.com>
2023-07-28 00:23:10 -0500

    update add item and cross out
    updated syntax

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,14 @@ function newItem(){
 
     //javascript
     //1. Adding a new item to the list of items: 
-       let li = document.createElement("li");
-       let inputValue = document.getElementById("input").value;
-       let text = document.createTextNode(inputValue);
-       li.appendChild(text);
+       let li = $("<li></li>");
+       let inputValue = $("#input").val();
+       li.append(inputValue);
     
        if (inputValue === '') {
-         alert("You must write something!");
+         alert("This cannot be blank!");
        } else {
-         let list = document.querySelector('#list');
-         list.appendChild(li);
+        $("#list").append(li);
        }
     
      //2. Crossing out an item from the list of items:
@@ -19,14 +17,17 @@ function newItem(){
              li.classList.toggle("strike");
          }
     
-         li.addEventListener("dblclick",crossOut);
+            li.on("dblclick", function crossout(){
+                li.toggleClass("strike")
+            });
     
      //3(i). Adding the delete button "X": 
        let crossOutButton = document.createElement("crossOutButton");
          crossOutButton.appendChild(document.createTextNode("X"));
          li.appendChild(crossOutButton);
-    
+         
          crossOutButton.addEventListener("click", deleteListItem);
+
      //3(ii). Adding CLASS DELETE (DISPLAY: NONE) from the css:
        function deleteListItem(){
              li.classList.add("delete")
